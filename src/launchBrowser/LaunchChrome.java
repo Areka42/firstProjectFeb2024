f37cd094f8d9fe0ba80f62d310cfa822d878ef99
Areka42 <areka.mcclean@gmail.com>
2024-08-03 13:50:13 -0500

    first commit

---
+++ src/launchBrowser/LaunchChrome.java
@@ -0,0 +1,16 @@
+package launchBrowser;
+
+import java.util.Scanner;
+
+public class LaunchChrome {
+
+		//variables/attributes/fields
+		//methods
+	
+		public static void main(String[] args) {
+
+			Scanner scn = new Scanner(System.in);
+				
+			WebDriver driver = new ChromeDriver();
+		}
+}
